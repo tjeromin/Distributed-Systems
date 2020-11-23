57876981b02b818b64b73a006889656bd5d79456
TinoJer <davidjer@hotmail.de>
2020-11-24 00:21:19 +0100

    Ring election working

--- Lab2/code_template/server/server.py
+++ Lab2/code_template/server/server.py
@@ -5,6 +5,8 @@ import sys
 from threading import Lock, Thread
 import time
 import traceback
+from typing import Any
+
 import bottle
 from bottle import Bottle, request, template, run, static_file
 import requests
@@ -33,10 +35,6 @@ class Blackboard:
 
     def set_content(self, index, new_content):
         with self.lock:
-            if index not in self.content:
-                self.counter += 1
-                if index is None:
-                    index = self.counter - 1
             self.content[index] = new_content
         return
 
@@ -55,23 +53,32 @@ class Server(Bottle):
         self.id = int(ID)
         self.ip = str(IP)
         self.rnd_number = random.randint(0, 10 ** 6)
-        # dictionary of server_ip, rnd_number pairs
-        self.servers_list = dict.fromkeys(servers_list, None)
+        # dictionary with shape server_ip: (rnd_number, next_ip)
+        self.servers_dict = dict.fromkeys(servers_list, (Any, Any))
+        # Bottle doesn't allow reassigning an attribute, so this is a workaround by changing the 1st element of the list
+        self.leader_ip = ['']
+        self.next_ip = ['']
+        if self.id < 8:
+            self.next_ip[0] = '10.1.0.' + str(self.id + 1)
+        else:
+            self.next_ip[0] = '10.1.0.1'
         # list all REST URIs
         # if you add new URIs to the server, you need to add them here
         self.route('/', callback=self.index)
-        self.post('/init', callback=self.post_init)
         self.get('/board', callback=self.get_board)
         self.post('/board', callback=self.post_board)
         self.post('/board/<element_id:int>/', callback=self.post_modify)
         # we give access to the templates elements
         self.get('/templates/<filename:path>', callback=self.get_template)
         self.post('/propagate', callback=self.post_propagate)
+        self.post('/leader_election', callback=self.post_leader_election)
         # You can have variables in the URI, here's an example self.post('/board/<element_id:int>/',
         # callback=self.post_board) where post_board takes an argument (integer) called element_id
-        self.do_parallel_task_after_delay(5, self.propagate_to_all_servers,
-                                          args=('/init', 'POST',
-                                                {'ip': self.ip, 'rnd_number': self.rnd_number}))
+        time.sleep(2)
+        print('id ' + str(self.id))
+        if self.id == 1:
+            print('sending')
+            self.start_leader_election()
 
     def do_parallel_task(self, method, args=None):
         # create a thread running a new task Usage example: self.do_parallel_task(self.contact_another_server,
@@ -96,10 +103,11 @@ class Server(Bottle):
         method(*args)
 
     def contact_another_server(self, srv_ip, URI, req='POST', params_dict=None):
-        # Try to contact another serverthrough a POST or GET
+        # Try to contact another server through a POST or GET
         # usage: server.contact_another_server("10.1.1.1", "/index", "POST", params_dict)
         success = False
         try:
+            res = 0
             if 'POST' in req:
                 res = requests.post('http://{}{}'.format(srv_ip, URI),
                                     data=params_dict)
@@ -112,30 +120,94 @@ class Server(Bottle):
             print("[ERROR] " + str(e))
         return success
 
+    def start_leader_election(self):
+        # put ip and random number in str separated by "|"
+        servers_str = str(self.ip) + '|' + str(self.rnd_number) + '|'
+        self.do_parallel_task(self.contact_another_server,
+                              args=(self.next_ip[0], '/leader_election', 'POST',
+                                    {'action': 'election', 'servers_dict': servers_str, 'initiator_ip': self.ip}))
+
+    def post_leader_election(self):
+        action = request.forms.get('action')
+        servers_str = request.forms.get('servers_dict')
+        init_ip = request.forms.get('initiator_ip')
+
+        if action == 'election':
+            print('election')
+            if init_ip == self.ip:
+                self.do_parallel_task(self.contact_another_server,
+                                      args=(self.next_ip[0], '/leader_election', 'POST',
+                                            {'action': 'coordination',
+                                             'servers_dict': servers_str,
+                                             'initiator_ip': init_ip}))
+            else:
+                servers_str += str(self.ip) + '|' + str(self.rnd_number) + '|'
+                self.do_parallel_task(self.contact_another_server,
+                                      args=(self.next_ip[0], '/leader_election', 'POST',
+                                            {'action': 'election',
+                                             'servers_dict': servers_str,
+                                             'initiator_ip': init_ip}))
+        elif action == 'coordination':
+            if servers_str[-1] == '|':
+                servers_str = servers_str[:-1]
+            print(servers_str)
+            servers_list = servers_str.split('|')
+            print(servers_list)
+            ip_list = servers_list[0::2]
+            print(ip_list)
+            rnd_list = [int(i) for i in servers_list[1::2]]
+            print(rnd_list)
+            # make a dictionary of shape server_ip: (rnd_number, next_ip)
+            # self.servers_dict = {ip_list[i]: (rnd_list[i], ip_list[(i + 1) % len(ip_list)])
+                                 # for i in range(0, len(ip_list))}
+            for i in range(0, len(ip_list)):
+                self.servers_dict[ip_list[i]] = (rnd_list[i], ip_list[(i + 1) % len(ip_list)])
+            # get a list of all indices of rnd_list where rnd_number max is
+            max_indices = [i for i, j in enumerate(rnd_list) if j == max(rnd_list)]
+            # get a list of IPs where rnd_number max is
+            max_ip_list = [ip_list[i] for i in max_indices]
+            # leader_ip is the max ip where rnd_number max is
+            self.leader_ip[0] = max(max_ip_list)
+            self.next_ip[0] = self.servers_dict[self.ip][1]
+            print('leader ' + self.leader_ip[0])
+
+            if self.ip != init_ip:
+                self.do_parallel_task(self.contact_another_server,
+                                      args=(self.next_ip[0], '/leader_election', 'POST',
+                                            {'action': 'coordination',
+                                             'servers_dict': servers_str,
+                                             'initiator_ip': init_ip}))
+
+
     def propagate_to_all_servers(self, URI='/propagate', req='POST', params_dict=None):
-        for srv_ip in self.servers_list:
+        for srv_ip in self.servers_dict:
             if srv_ip != self.ip:  # don't propagate to yourself
                 success = self.contact_another_server(srv_ip, URI, req, params_dict)
                 if not success:
                     print("[WARNING ]Could not contact server {}".format(srv_ip))
 
-    def post_init(self):
-        rnd_number = request.forms.get('rnd_number')
-        ip = request.forms.get('ip')
-        self.servers_list[ip] = rnd_number
-        print('server ' + str(ip) + ': ' + str(rnd_number))
+    def propagate_to_leader(self, URI, req='POST', params_dict=None):
+        success = self.contact_another_server(self.leader_ip[0], URI, req, params_dict)
+        if not success:
+            print('[WARNING ]Could not contact leader.')
+            # initiate election
 
     # post to ('/propagate')
     def post_propagate(self):
-        element_id = request.forms.get('element_id')
-        entry = request.forms.get('entry')
-        delete = request.forms.get('delete')
-        if delete == '1':
-            self.blackboard.del_content(int(element_id))
-        elif element_id is None:
-            self.blackboard.add_content(new_entry=entry)
-        else:
-            self.blackboard.set_content(index=int(element_id), new_content=entry)
+        action = request.forms.get('action')
+        # only at leader
+        if action == 'submit':
+            print("at leader")
+            entry = request.forms.get('entry')
+            # waiting for all to return when spreading content
+            self.propagate_to_all_servers('/propagate', 'POST',
+                                          {'action': 'sync_content', 'sync_content': entry})
+            self.blackboard.add_content(entry)
+        # only at follower
+        elif action == 'sync_content':
+            print("at follower")
+            sync_content = request.forms.get('sync_content')
+            self.blackboard.add_content(sync_content)
 
     # route to ('/')
     def index(self):
@@ -164,11 +236,10 @@ class Server(Bottle):
         try:
             # we read the POST form, and check for an element called 'entry'
             new_entry = request.forms.get('entry')
-            self.blackboard.add_content(new_entry=new_entry)
             print("Received: {}".format(new_entry))
-            self.do_parallel_task(self.propagate_to_all_servers,
+            self.do_parallel_task(self.propagate_to_leader,
                                   args=('/propagate', 'POST',
-                                        {'delete': '0', 'element_id': None, 'entry': new_entry}))
+                                        {'action': 'submit', 'entry': new_entry}))
         except Exception as e:
             print("[ERROR] " + str(e))
 
